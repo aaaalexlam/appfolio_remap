f94b1c78add64fe5a9f0ca0d35de207ac6f16fc0
DESKTOP-EVOIAT9\Alex Lam <alexlamwk0305@gmail.com>
2025-05-02 10:49:52 -0700

    - update vendor lendger

--- report/vendorLedger/vendorLendger.js
+++ report/vendorLedger/vendorLendger.js
@@ -15,55 +15,7 @@ function initTable() {
     columns.forEach((column) => {
         table.innerHTML += columnHeaderTemplate(column.name);
         columnCheckbox.innerHTML += columnCheckboxTemplate(column.name);
-
     });
 
-
-    const resizers = document.querySelectorAll(".resize_div");
-    let currentResizer;
-
-    resizers.forEach(resizer => {
-        resizer.addEventListener("mousedown", mousedownHandler);
-
-        function mousedownHandler(e) {
-            currentResizer = resizer;
-            let main_container_header = currentResizer.parentElement;
-            let startX = e.pageX;
-            let startWidth = main_container_header.offsetWidth;
-
-            function mousemoveHandler(e) {
-                const newWidth = startWidth + (e.pageX - startX);
-                main_container_header.style.width = newWidth + "px";
-            }
-
-            function mouseupHandler() {
-                document.removeEventListener("mousemove", mousemoveHandler);
-                document.removeEventListener("mouseup", mouseupHandler);
-            }
-
-            document.addEventListener("mousemove", mousemoveHandler);
-            document.addEventListener("mouseup", mouseupHandler);
-        }
-    })
-}
-
-function handleCheckboxClick(event) {
-    const isChecked = event.target.checked;
-    const value = event.target.value;
-    const column = document.getElementById(`main_container_${toSnakeCase(value)}`)
-
-    if (isChecked) {
-        column.style.display = 'none'
-    } else {
-        column.style.display = 'block'
-    }
-}
-
-
-
-function toSnakeCase(str) {
-    return str
-        .trim()                   // remove leading/trailing spaces
-        .toLowerCase()            // convert to lowercase
-        .replace(/\s+/g, '_');    // replace spaces with underscores
+    initResizeColumn();
 }
